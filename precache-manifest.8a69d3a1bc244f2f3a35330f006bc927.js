66b1f1630dc576fd6ee4ac6c40b17de4e55d25a9
estefanoo <estefanocarlos54@gmail.com>
2020-09-29 17:44:01 -0300

    Updates

---
+++ precache-manifest.8a69d3a1bc244f2f3a35330f006bc927.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdd67a891a786b03cbc6b5cc55286e3",
+    "url": "/app-netflix/index.html"
+  },
+  {
+    "revision": "0b5e0d226c844652c4ee",
+    "url": "/app-netflix/static/css/main.8c290a29.chunk.css"
+  },
+  {
+    "revision": "b3eb010240e846d0f06f",
+    "url": "/app-netflix/static/js/2.86bb1a85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-netflix/static/js/2.86bb1a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5e0d226c844652c4ee",
+    "url": "/app-netflix/static/js/main.5341f25b.chunk.js"
+  },
+  {
+    "revision": "3b4f980a8c59eefcddf1",
+    "url": "/app-netflix/static/js/runtime-main.c80c00a2.js"
+  },
+  {
+    "revision": "92359d645d389426766dbfdfd8875c29",
+    "url": "/app-netflix/static/media/bell.92359d64.svg"
+  },
+  {
+    "revision": "f322865ea843d6a66157f6da7a2c4ac8",
+    "url": "/app-netflix/static/media/gift.f322865e.svg"
+  },
+  {
+    "revision": "0b1a6e63bb1e10e413d4edcc59543f72",
+    "url": "/app-netflix/static/media/github.0b1a6e63.svg"
+  },
+  {
+    "revision": "1e3b73767bcc51b5f3059c1b2cf1b201",
+    "url": "/app-netflix/static/media/linkedin.1e3b7376.svg"
+  },
+  {
+    "revision": "a1f673e7df715f16dae49f4874009082",
+    "url": "/app-netflix/static/media/netflix_logo.a1f673e7.png"
+  },
+  {
+    "revision": "96bf0545b1330a1b9ed1f9bfbae03746",
+    "url": "/app-netflix/static/media/search.96bf0545.svg"
+  },
+  {
+    "revision": "79a9f664913611c25e0de0429f2529f3",
+    "url": "/app-netflix/static/media/simple-left.79a9f664.svg"
+  },
+  {
+    "revision": "6206c07d93a3a4738591a407eb0fe6b2",
+    "url": "/app-netflix/static/media/simple-right.6206c07d.svg"
+  }
+]);
